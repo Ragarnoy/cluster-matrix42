1c1fd7b9d61f6c7ce902f0eda764efa4b5cd1531
ragarnoy <tiagolernould@gmail.com>
2025-07-05 22:59:41 +0200

    annoying pos

--- applications/cluster-matrix/src/lib.rs
+++ applications/cluster-matrix/src/lib.rs
@@ -4,9 +4,7 @@
 extern crate std;
 
 // Re-export the separated crates for backwards compatibility
-pub use cluster_core as cluster;
 pub use graphics_common as graphics;
 
 // Legacy re-exports to maintain compatibility
-pub use cluster_core::{constants, shared, visualization};
 pub use graphics_common::{animations, utilities};
